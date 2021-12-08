dd310bff5e25a34f0d50e55137d9a369cf7cc7ae
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-12-08 13:58:02 -0600

    Finally expand operator to fetch all planets ! ! !
    EMPTY too ! ! !

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, pipe, merge } from 'rxjs';
-import { repeat, tap, map } from 'rxjs/operators';
+import { Observable, pipe, merge, EMPTY } from 'rxjs';
+import { repeat, tap, map, expand } from 'rxjs/operators';
 
 export interface SwapiPlanetDataWeCareAbout {
   next: string;
@@ -26,6 +26,7 @@ export class SwapiService {
       .pipe(
         // repeat(3)
         tap(x => console.log(x))
+        , expand(x => x.next ? this.httpSvc.get<SwapiPlanetDataWeCareAbout>(x.next) : EMPTY)
         , map(x => ({
           next: x.next
           , results: x.results.map(y => ({ name: y.name }))
@@ -34,8 +35,9 @@ export class SwapiService {
       )
     ;
 
-    const page2 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>("https://swapi.dev/api/planets/?page=2")
-  
-    return merge(page1, page2);
+    return page1; 
+
+    // const page2 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>("https://swapi.dev/api/planets/?page=2")
+    //return merge(page1, page2);
   }; 
 }
